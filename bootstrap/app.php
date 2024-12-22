6ffe0614127912ef4862c1e590a93fb0d4a2f89e
coldtothetouch <coldtothetouch@dirtyanimals.ru>
2024-12-22 17:48:25 +0300

    Rate limiter fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,18 +3,16 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Routing\Middleware\ThrottleRequests;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
-//        then: function () {
-//
-//        }
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append('throttle:global');
+        $middleware->append(ThrottleRequests::class.':global');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
