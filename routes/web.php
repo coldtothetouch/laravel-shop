721311e025b0ab1652739cfbb6df275fcc19923d
moiseevgleb <moiseevglebalex@yandex.ru>
2025-01-05 17:15:24 +0300

    Implemented github authentication

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,18 @@ use App\Http\Controllers\Auth\ResetPasswordController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', HomeController::class)->name('home');
 
-Route::as('auth.')->group(function () {
+Route::as('auth.')->middleware('guest')->group(function () {
     Route::controller(LoginController::class)->group(function () {
         Route::get('/login', 'index')->name('login.index');
         Route::post('/login', 'store')->name('login');
-        Route::delete('/logout', 'destroy')->name('logout');
+
+        Route::delete('/logout', 'destroy')
+            ->withoutMiddleware('guest')
+            ->middleware('auth')
+            ->name('logout');
     });
 
     Route::controller(RegisterController::class)->group(function () {
@@ -21,9 +26,14 @@ Route::as('auth.')->group(function () {
     });
 
     Route::controller(ResetPasswordController::class)->group(function () {
-        Route::get('/reset-password', 'resetPassword')->name('reset-password.index');
-        Route::post('/reset-password', 'store')->name('reset-password');
         Route::get('/forgot-password', 'forgotPassword')->name('forgot-password.index');
         Route::post('/forgot-password', 'sendEmail')->name('send-email');
     });
+
+    Route::get('/auth/github/redirect', [LoginController::class, 'github'])->name('github.redirect');
+    Route::get('/auth/github/callback', [LoginController::class, 'githubCallback'])->name('github.callback');
 });
+Route::post('/update-password', [ResetPasswordController::class, 'store'])->name('password.update');
+Route::get('/reset-password/{token}', [ResetPasswordController::class, 'resetPassword'])->name('password.reset');
+
+
